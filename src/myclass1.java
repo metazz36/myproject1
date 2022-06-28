b07f751c3fc2aabb7aa46d5c9c4f5fb26268b03c
metazz <772150928@qq.com>
2022-06-28 12:24:32 +0800

    第二次提交

--- src/myclass1.java
+++ src/myclass1.java
@@ -1,6 +1,7 @@
 public class myclass1 {
     public static void main(String[] args) {
         System.out.println("111");
-        System.out.println("123");
+        System.out.println("222");
+        System.out.println("333");
     }
 }

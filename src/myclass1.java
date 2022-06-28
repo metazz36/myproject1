4c9c80b953612b6a268d6f1d9b2a8ac95923c175
metazz36 <90548160+metazz36@users.noreply.github.com>
2022-06-28 13:53:01 +0800

    github commit1

--- src/myclass1.java
+++ src/myclass1.java
@@ -8,5 +8,6 @@ public class myclass1 {
         System.out.println("master test");
         System.out.println("master push");
         System.out.println("1111");
+        System.out.println("2222");
     }
 }

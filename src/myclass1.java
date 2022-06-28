ae6b5f5c59f773da0e08c83e752898338ff15564
metazz <772150928@qq.com>
2022-06-28 13:57:28 +0800

    commit after cloning

--- src/myclass1.java
+++ src/myclass1.java
@@ -10,5 +10,6 @@ public class myclass1 {
         System.out.println("1111");
         System.out.println("2222");
         System.out.println("3333");
+        System.out.println("4444");
     }
 }
